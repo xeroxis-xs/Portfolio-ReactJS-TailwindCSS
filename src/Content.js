07859cd1900e63300210843cfe7e2b683ac83195
Teoh Xi Sheng <txsheng0609@hotmail.com>
2023-05-19 13:16:02 +0800

    Fix projects to Projects

--- src/Content.js
+++ src/Content.js
@@ -19,10 +19,10 @@ import ocbc from "./assets/images/Experiences/ocbc.png";
 import starhub from "./assets/images/Experiences/starhub.png";
 import spf from "./assets/images/Experiences/spf.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+import project1 from "./assets/images/Projects/img1.png";
+import project2 from "./assets/images/Projects/img2.png";
+import project3 from "./assets/images/Projects/img3.png";
+import person_project from "./assets/images/Projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
